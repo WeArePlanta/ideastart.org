1a69e86289da6cf75b81db8e09eda6499a5678fa
natmacira <natimciraolo@gmail.com>
2022-10-26 16:51:34 -0300

    contador mobile

--- js/main.js
+++ js/main.js
@@ -1,23 +1,86 @@
 
-       var upgradeTime = 172801;
-            var seconds = upgradeTime;
-            function timer() {
-                var days = Math.floor(seconds / 24 / 60 / 60);
-                var hoursLeft = Math.floor((seconds) - (days * 86400));
-                var hours = Math.floor(hoursLeft / 3600);
-                var minutesLeft = Math.floor((hoursLeft) - (hours * 3600));
-                var minutes = Math.floor(minutesLeft / 60);
-                var remainingSeconds = seconds % 60;
-                function pad(n) {
-                    return (n < 10 ? "0" + n : n);
-                }
-                document.getElementById('countdown').innerHTML = pad(days) + ":" + pad(hours) + ":" + pad(minutes) + ":" + pad(remainingSeconds);
-                if (seconds == 0) {
-                    clearInterval(countdownTimer);
-                    document.getElementById('countdown').innerHTML = "Completed";
-                } else {
-                    seconds--;
-                }
-            }
-            var countdownTimer = setInterval('timer()', 1000);
-      
+document.addEventListener("DOMContentLoaded", function(event) {
+
+const today      = new Date();
+console.log(today);
+
+const daysLeft = Math.floor( (  ( new Date("2022-11-20T00:00:00.000-05:00") ) - today ) / ( 1000 * 60 * 60 * 24 ) )
+console.log(daysLeft);
+
+const hours = Math.floor( (today/(1000*60*60)) % 24 );
+console.log(hours);
+
+const minutes = Math.floor( (today/1000/60) % 60 );
+console.log(minutes);
+
+const seconds = Math.floor( (today/1000) % 60 );
+console.log(seconds);
+
+//acá creo los elementos html correspondientes para cada valor
+
+//dias
+var diaTexto = document.createElement("p");
+var diaSpan = document.createElement("span");
+//horas
+var hoursTexto = document.createElement("p");
+var hoursSpan = document.createElement("span");
+//minutos
+var minutesTexto = document.createElement("p");
+var minutesSpan = document.createElement("span");
+//segundos
+var secondsTexto = document.createElement("p");
+var secondsSpan = document.createElement("span");
+
+//acá creo los textos que iran dentro de los elementos html
+
+//dias
+var text = document.createTextNode(daysLeft);
+var textDiaSpan = document.createTextNode("Días");
+//horas
+var textHoras = document.createTextNode(hours);
+var textHorasSpan = document.createTextNode("Horas");
+//minutos
+var textMinutos = document.createTextNode(minutes);
+var textMinutosSpan = document.createTextNode("Minutos");
+//segundos
+var textSegundos = document.createTextNode(seconds);
+var textSegundosSpan = document.createTextNode("Segundos");
+
+
+//acá agrego los textos creados a los elementos creados 
+//dias
+diaTexto.appendChild(textHoras);
+diaSpan.appendChild(textDiaSpan); 
+//horas
+hoursTexto.appendChild(text);
+hoursSpan.appendChild(textHorasSpan); 
+//minutos
+minutesTexto.appendChild(textMinutos);
+minutesSpan.appendChild(textMinutosSpan); 
+//segundos
+secondsTexto.appendChild(textSegundos);
+secondsSpan.appendChild(textSegundosSpan); 
+
+//acá llamo al div que ya existe en el html y le agrego a cada uno sus elementos correspondientes
+//dias
+var divUno = document.getElementById('div-uno');
+divUno.appendChild(diaTexto);
+divUno.appendChild(diaSpan);
+//horas
+var divDos = document.getElementById('div-dos');
+divDos.appendChild(hoursTexto);
+divDos.appendChild(hoursSpan);
+//minutos
+var divTres = document.getElementById('div-tres');
+divTres.appendChild(minutesTexto);
+divTres.appendChild(minutesSpan);
+//segundos
+var divCuatro = document.getElementById('div-cuatro');
+divCuatro.appendChild(secondsTexto);
+divCuatro.appendChild(secondsSpan);
+
+
+});
+
+
+
